53cd58c7fdad881336d1938d8a1d25a9e5aabefa
MoonManBoolin <jellyeater343@gmail.com>
2025-09-12 14:08:55 -0700

    updated for github pages

--- src/generatepage.py
+++ src/generatepage.py
@@ -3,7 +3,7 @@ from extracttitle import extract_title
 from markdowntohtmlnode import markdown_to_html_node
 
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     with open(from_path, "r", encoding="utf-8") as f:
         md = f.read()
@@ -11,10 +11,10 @@ def generate_page(from_path, template_path, dest_path):
         template = f.read()
     title = extract_title(md)
     html_content = markdown_to_html_node(md)
-    print(html_content)
     html_content = html_content.to_html()
     
     html = template.replace("{{ Title }}", title).replace("{{ Content }}", html_content)
+    html = html.replace('href="/', f'href="{basepath}').replace('src="/', f'src="{basepath}')
     dest_path = pathlib.Path(dest_path)
     dest_path.parent.mkdir(parents=True, exist_ok=True)
     with open(dest_path, "w", encoding="utf-8") as f:
--- src/main.py
+++ src/main.py
@@ -1,12 +1,16 @@
 import os
 import pathlib
 import shutil
+import sys
 from copystatic import copy_contents_helper
 from generatepages import generate_pages_recursive
 
 
 def main():
-    public = "./public"
+    print(sys.argv)
+    basepath = sys.argv[1] if len(sys.argv) > 1 else "/"
+    print(basepath)
+    public = "./docs"
     static = "./static"
     content_md = "./content"
     template_path = "./template.html"
@@ -16,8 +20,9 @@ def main():
         except OSError as e:
             print(f"Error deleting folder: {e}")
     pathlib.Path(public).mkdir(parents=True, exist_ok=True)
+    print("COPYING CONTENTS AND GENERATING PAGES...")
     copy_contents_helper(static, public)
-    generate_pages_recursive(content_md, template_path, public)
+    generate_pages_recursive(content_md, template_path, public, basepath)
     
 if __name__ == "__main__":
     main()
